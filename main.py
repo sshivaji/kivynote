f70e5f054667d64e3a0e08dfaed61d1033e6b704
sshivaji <sshivaji@gmail.com>
2016-03-01 01:28:48 -0800

    quick erase

--- main.py
+++ main.py
@@ -6,16 +6,23 @@ from kivy.graphics import Color, Ellipse, Line
 from kivy.properties import ListProperty
 from kivy.core.window import Window
 from kivy.utils import get_color_from_hex
-
+from kivy.uix.boxlayout import BoxLayout
 
 Window.clearcolor = (1, 1, 1, 1)
-
+erase_color = (1,1,1,1)
 class KivyDraw(Widget):
-
+    def __init__(self, **kwargs):
+        super(KivyDraw, self).__init__(**kwargs)
+        self.color = None
+    
     def on_touch_down(self, touch):
-        color = (random(), 1, 1)
+        if not self.color:
+            color = (random(), random(), random())
+            print(color)
+        else:
+            color = self.color
         with self.canvas:
-            Color(*color, mode='hsv')
+            Color(*color)
             d = 1.
             Ellipse(pos=(touch.x - d / 2, touch.y - d / 2), size=(d, d))
             touch.ud['line'] = Line(points=(touch.x, touch.y), width=3)
@@ -31,16 +38,26 @@ class KivyNoteBookApp(App):
         self.painter = KivyDraw()
         clearbtn = ColorButton(text='Clear', shorten=True)
         clearbtn.bind(on_release=self.clear_canvas)
+       
+        erasebtn = ColorButton(text='Erase', shorten=True)
+        erasebtn.bind(on_release=self.set_erase) 
         parent.add_widget(self.painter)
-        parent.add_widget(clearbtn)
+        layout = BoxLayout(orientation = 'horizontal', padding=5, spacing=5)
+        layout.add_widget(clearbtn)
+        layout.add_widget(erasebtn)
+        parent.add_widget(layout)
         return parent
 
     def clear_canvas(self, obj):
         self.painter.canvas.clear()
+        self.painter.color = None
+
+    def set_erase(self, obj):
+        self.painter.color = erase_color
 
 class ColorButton(Button):
     def __init__(self, **kwargs):
-        super(ColorDownButton, self).__init__(**kwargs)
+        super(ColorButton, self).__init__(**kwargs)
         self.background_normal = ""
         self.background_down = ""
         self.background_color = get_color_from_hex('#000080')
