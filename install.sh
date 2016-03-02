0335475c05399af13145aefa559c4fafb6257cbc
sshivaji <sshivaji@gmail.com>
2016-03-01 23:42:40 -0800

    python venv for kivy

---
+++ install.sh
@@ -0,0 +1,8 @@
+python virtualenv.py --system-site-packages drawpython
+drawpython/bin/pip install setuptools --no-use-wheel --upgrade
+drawpython/bin/pip install cython==0.20.2
+drawpython/bin/pip install hg+http://bitbucket.org/pygame/pygame
+drawpython/bin/pip install kivy==1.9.1
+drawpython/bin/pip install git+https://github.com/kivy/buildozer.git@master
+drawpython/bin/pip install git+https://github.com/kivy/plyer.git@master
+drawpython/bin/pip install -U pygments docutils
